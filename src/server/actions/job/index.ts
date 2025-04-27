86a61eb9dd2b2905aa6fca4007014af9ebcebdfd
Mr.Kashan <kashisial2327@gmail.com>
2025-04-26 20:47:14 -0700

    commit

--- src/server/actions/job/index.ts
+++ src/server/actions/job/index.ts
@@ -6,13 +6,13 @@ import { Prisma } from "@prisma/client";
 const jobRepository = new JobRepository();
 
 export async function createJob(data: Prisma.JobCreateInput) {
-	return jobRepository.create(data);
+  return jobRepository.create(data);
 }
 
 export async function updateJob(id: string, data: Prisma.JobUpdateInput) {
-	return jobRepository.update(id, data);
+  return jobRepository.update(id, data);
 }
 
 export async function deleteJob(id: string) {
-	return jobRepository.delete(id);
+  return jobRepository.delete(id);
 }
